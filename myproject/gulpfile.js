6c317445471c49c91f7f40ecc886645bdc5906d4
artemderzkiy <amironich1993@gmail.com>
2016-07-04 17:37:43 +0400

    bestversionwithoutfonts

--- myproject/gulpfile.js
+++ myproject/gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('css-libs', ['sass'], function() {
 });
 
 gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
-    gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('app/css/**/*.css', ['sass']); // Наблюдение за sass файлами в папке sass
     gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
